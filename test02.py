56757b274fea6e0ca3314de6de0dd7695539a8ff
MyHiHi <909535692@qq.com>
2019-02-12 22:51:43 +0800

    test

--- test02.py
+++ test02.py
@@ -1,3 +1,10 @@
 from functools import reduce
-def test():
-    pass
+# def test():
+#     pass
+
+import os;
+from collections import Counter,deque,defaultdict,namedtuple
+# print(os.path.isfile("test.cpp"))
+
+# with open("test.cpp","rb") as f:
+#     print(Counter(f))
