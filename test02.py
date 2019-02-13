d9d58e5bd8cc7a8ccd2f41a0d38449d1cf21881e
MyHiHi <909535692@qq.com>
2019-02-13 08:57:56 +0800

    test

--- test02.py
+++ test02.py
@@ -13,3 +13,25 @@ animal = namedtuple("animal","age name")
 test = animal("huaer",11)
 print(test.name)
 
+# for i in dir():
+#     print(dir(i))
+# class test(object):
+#     def __init__(self):
+#         pass
+#     def get(self):
+#         print()
+# print(dir(test()))
+
+def get_member(func):
+    from functools import wraps
+    @wraps(func)
+    def wrapper(*args,**kwargs):
+        from  inspect import getmembers
+        print(args[0] +":: ",getmembers(args[0])==dir(args[0]),"\n") 
+        print(func.__name__+": ",getmembers(func)==dir(func),"\n")
+    return wrapper
+
+@get_member
+def exam(str):
+    pass
+exam("销售")
